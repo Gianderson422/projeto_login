c1f021e8d678403f56bc1a5118ce4949942bdb70
Gianderson <giandozoretz@gmail.com>
2026-01-02 22:00:04 -0300

    Atualização do site: nova versão do projeto

---
+++ public/js/script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const signUpLink = document.querySelector(".login-form-subscribe a");
+    const loginLink = document.querySelector(".sign-up-form-login a");
+
+    const showFormSignUp = () => {
+        document.querySelector(".login-form").style.display = "none";
+        document.querySelector(".sign-up-form").style.display = "flex";
+        document.querySelector(".login-text-content h2").innerHTML =
+        "Olá! <br />Seja bem-vindo(a).";
+
+    };
+
+    const showFormLogin = () => {
+        document.querySelector(".login-form").style.display = "flex";
+        document.querySelector(".sign-up-form").style.display = "none";
+        document.querySelector(".login-text-content h2").innerHTML =
+        "Olá! <br />Seja bem-vindo(a) de volta.";
+
+    };
+
+    signUpLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        showFormSignUp();
+
+    });
+
+    loginLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        showFormLogin();
+
+    });
+});
